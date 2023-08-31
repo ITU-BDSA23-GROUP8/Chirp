1563e1a677e634e21f912c93559669545aec1328
Astrid <astb@itu.dk>
2023-08-31 16:03:04 +0200

    started reading the csv

---
+++ Chirp.CLI/Cheep.cs
@@ -0,0 +1,23 @@
+using System.Reflection.Metadata;
+
+class Cheep{
+    string user; 
+    string message; 
+    DateTimeOffset timestamp;
+
+    public Cheep(string user, string message, DateTimeOffset timestamp) {
+        this.user = user; 
+        this.message = message;
+        this.timestamp = timestamp; 
+
+    }
+
+    public override string ToString()
+    {
+        return $"{user} @ {timestamp}: {message}";
+    }
+
+
+
+
+}
--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    
+    public static void Main()
+    {
+      List<Cheep> cheeps = new List<Cheep>();  
+        try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("chirp_cli_db.csv"))
+            {
+                string cheep; 
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    cheep = sr.ReadLine();
+                    var values = Regex.Split(cheep, ",(?=(?:[^']*'[^']*')*[^']*$)");
+                    cheeps.Add(new Cheep(values[0], values[1], DateTimeOffset.Parse(values[2])));
+                }
+
+                // Read the stream as a string, and write the string to the console.
+                foreach (var item in cheeps)
+                {
+                   Console.WriteLine(item);
+                }
+               
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
