890be0036c8fbffce58ac1530c04459c96bce049
Clara Sønderborg <auso@itu.dk>
2023-09-19 15:46:33 +0200

    First testmethod added to SimpleDB.Tests

--- test/SimpleDB.Tests/UnitTest1.cs
+++ test/SimpleDB.Tests/UnitTest1.cs
@@ -2,9 +2,23 @@ namespace SimpleDB.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    record Data(string data1, string data2, long data3);
+    
 
+    //test that we can read a CSV-file and get the correct amount of records
+    [Fact]
+    public void TestReadCSV1()
+    {
+        //arrange
+        CSVDatabase<Data> database = CSVDatabase<Data>.GetInstance("../../../test_data.csv");
+
+        //act
+        var recordList = database.Read();
+
+        //assert
+        Assert.Equal<int>(2, recordList.Count());
     }
+
+
 }
+
