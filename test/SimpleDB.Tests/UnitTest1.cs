c775ffd3143842a09f2c4119d381e8d51205127c
Clara Sønderborg <auso@itu.dk>
2023-09-19 16:01:54 +0200

    Fix testmethod 1

--- test/SimpleDB.Tests/UnitTest1.cs
+++ test/SimpleDB.Tests/UnitTest1.cs
@@ -3,6 +3,7 @@ namespace SimpleDB.Tests;
 public class UnitTest1
 {
     record Data(string data1, string data2, long data3);
+    CSVDatabase<Data> database;
     
 
     //test that we can read a CSV-file and get the correct amount of records
@@ -10,8 +11,8 @@ public class UnitTest1
     public void TestReadCSV1()
     {
         //arrange
-        CSVDatabase<Data> database = CSVDatabase<Data>.GetInstance("../../../test_data.csv");
-
+        database = CSVDatabase<Data>.GetInstance("../../../test_data.csv");
+        
         //act
         var recordList = database.Read();
 
