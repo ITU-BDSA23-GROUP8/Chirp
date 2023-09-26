0c087f366fe2e47d439d8918da715612c7a868ab
Clara Sønderborg <auso@itu.dk>
2023-09-26 14:09:12 +0200

    add unittest for simpleDB to assert POST request status code is 200

--- test/SimpleDB.Tests/DBUnitTest.cs
+++ test/SimpleDB.Tests/DBUnitTest.cs
@@ -5,8 +5,10 @@ using System.Net;
 
 namespace SimpleDB.Tests;
 
-public class UnitTest
+public class DBUnitTest
 {
+    public record Cheep(string Author, string Message, long Timestamp);
+
     [Fact]
     public async Task TestGetCheeps()
     {
@@ -16,8 +18,8 @@ public class UnitTest
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         client.BaseAddress = new Uri(baseURL);
-       
-        
+
+
 
         //act
         var cheeps = await client.GetFromJsonAsync<IEnumerable<Cheep>>("cheeps");
@@ -26,9 +28,28 @@ public class UnitTest
         //assert
         Assert.True(cheeps.Any());
         Assert.Equal(HttpStatusCode.OK, httpRespons.StatusCode);
-        
+
     }
 
+    [Fact]
+    public async Task TestPostCheeps()
+    {
+        //arrange
+        var baseURL = "http://localhost:5079";
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        client.BaseAddress = new Uri(baseURL);
+
+        //act
+        Cheep cheep = new("test", "test msg", 123456789);
+        var content = JsonContent.Create(cheep);
+        var response = await client.PostAsync("/cheep", content);
+
+        //assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+    }
 
 }
 
