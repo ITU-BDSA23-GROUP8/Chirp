ce8e4dfd047ec8c4aed84153a07286f5c5b9d028
AnnikaJensby <118458497+AnnikaJensby@users.noreply.github.com>
2023-10-17 13:35:04 +0200

    Extend repository, with find and create methods Co-authored-by: Clara <auso@itu.dk> Co-authored-by: Sarah <sscv@itu.dk> Co-authored-by: Astrid <astb@itu.dk> Co-authored-by: Julia <jukl@itu.dk>

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -1,9 +1,17 @@
 namespace Chirp.Core;
 
-public interface ICheepRepository{
-    public Task<IEnumerable<CheepDTO>> GetCheeps(int page, int offset); 
+public interface ICheepRepository
+{
+    public Task<IEnumerable<CheepDTO>> GetCheeps(int page, int offset);
 
-    public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthor(string user, int page, int offset); 
-    
+    public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthor(string user, int page, int offset);
+
+    public Task<AuthorDTO?> GetAuthorFromName(string user);
+
+    public Task<AuthorDTO?> GetAuthorFromEmail(string email);
+
+    public void CreateAuthor(AuthorDTO author);
+
+    public void CreateCheep(CheepDTO cheep, AuthorDTO author);
 
 }
--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -2,8 +2,8 @@ namespace Chirp.Infrastructure;
 
 public class Author{
     public int AuthorId {get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
     public List<Cheep>Cheeps { get; set;}
 
 
