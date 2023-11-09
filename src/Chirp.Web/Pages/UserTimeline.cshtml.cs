794c88b1350cc446dc99bf4f2b77442525645c63
Astrid <astb@itu.dk>
2023-11-09 15:39:12 +0100

    fixed previous and next for usertimline Co-authored-by: Annika <alyt@itu.dk> Co-authored-by: Julia <jukl@itu.dk>

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -15,18 +15,21 @@ public class UserTimelineModel : PageModel
         _repository = repo;
     }
 
+    public int pageRequest {get; set;}
     public async Task<ActionResult> OnGet(string author)
     {
+        
+        
         // https://learn.microsoft.com/en-us/dotnet/api/system.web.httprequest.querystring?view=netframework-4.8.1
         // used when looking for a specific page in the url, e.g. ?page=12
-        var urlRequest = Convert.ToInt32(Request.Query["page"]);
+        pageRequest = Convert.ToInt32(Request.Query["page"]);
 
         // to start on page 1
-        if (urlRequest == 0) {
-            urlRequest = 1;
+        if (pageRequest == 0) {
+            pageRequest = 1;
         }
 
-        var cheeps = await _repository.GetCheepsFromAuthor(author, urlRequest, (urlRequest - 1) * 32);
+        var cheeps = await _repository.GetCheepsFromAuthor(author, pageRequest, (pageRequest - 1) * 32);
         Cheeps = cheeps.ToList();
         return Page();
     }
