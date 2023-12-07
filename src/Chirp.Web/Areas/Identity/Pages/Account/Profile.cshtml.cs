d1fea0ea8d7490b6fb8b0143e3fc4df36e2fd8ed
Clara Sønderborg <auso@itu.dk>
2023-12-07 13:06:57 +0100

    About me page added Co-authored-by: Astrid <astb@itu.dk> Co-authored-by: Julia <jukl@itu.dk>

--- src/Chirp.Web/Areas/Identity/Pages/Account/Profile.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Profile.cshtml.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using Chirp.Core;
+using Chirp.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +8,40 @@ namespace MyApp.Namespace
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        public List<AuthorDTO> Following { get; set; }
+        public required List<CheepDTO> Cheeps { get; set; }
+        private readonly ICheepRepository _repository;
+        private readonly IAuthorRepository _authorrepository;
+
+        public string Name;
+
+        public string Email;
+
+        public ProfileModel(ICheepRepository repo, IAuthorRepository authorrepo)
         {
-            
+            _repository = repo;
+            _authorrepository = authorrepo;
+        }
+
+        public async Task OnGetAsync()
+        {
+            if (User.Identity!.IsAuthenticated)
+            {
+                var following = await _authorrepository.GetFollowing(new AuthorDTO(User.Identity.Name, User.Identity.Name));
+                Following = following.ToList();
+                
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+                var emailClaim = claimsIdentity!.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+                Email = emailClaim!.Value;
+                
+                var nameClaim = claimsIdentity.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
+                Name = nameClaim!.Value;
+
+                var cheeps = await _repository.GetAllCheepsFromAuthor(Name);
+                Cheeps = cheeps.ToList();
+
+            }
+
         }
     }
 }
