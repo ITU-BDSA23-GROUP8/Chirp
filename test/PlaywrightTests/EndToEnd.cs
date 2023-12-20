b3be152613592983216e2cea16d2dec1d2ea8a78
Astrid <astb@itu.dk>
2023-12-20 13:51:30 +0100

    updated playwright test to include like

--- test/PlaywrightTests/EndToEnd.cs
+++ test/PlaywrightTests/EndToEnd.cs
@@ -11,8 +11,7 @@ public class Tests : PageTest
 {
 
     [Test]
-    public async Task LoginWriteCheepLogout()
-    {
+    public async Task LoginWriteCheepLogout() {
         await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false,
@@ -35,11 +34,9 @@ public class Tests : PageTest
 
         await page.GetByRole(AriaRole.Textbox).ClickAsync();
 
-        await page.GetByRole(AriaRole.Textbox).FillAsync("Hej, dette er en test");
+        await page.GetByRole(AriaRole.Textbox).FillAsync("Dette er en test");
 
         await page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
-
-        await page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
 
         await page.GetByRole(AriaRole.Link, new() { Name = "logout" }).ClickAsync();
 
@@ -47,9 +44,10 @@ public class Tests : PageTest
 
     }
 
+
     [Test]
-     public async Task FollowAndUnfollow()
-    {
+     public async Task FollowAndLikeAndUnfollow(){
+        
         await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false,
@@ -70,17 +68,24 @@ public class Tests : PageTest
 
         await page.GetByLabel("Password").PressAsync("Enter");
 
-        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 2023-08-01 13:17:" }).GetByRole(AriaRole.Button).ClickAsync();
+        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 2023-08-01" }).Locator("button[name=\"AuthorEmail\"]").ClickAsync();
 
         await page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
 
-        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Unfollow Starbuck now is what we hear the worst. — 2023-08-01 13:17:" }).GetByRole(AriaRole.Button).ClickAsync();
+        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Unfollow The train pulled up at his bereavement; but his" }).Locator("button[name=\"cheepID\"]").ClickAsync();
 
-    }
+        await page.GetByRole(AriaRole.Link, new() { Name = "JuliaKlompmaker's profile" }).ClickAsync();
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "Jacqualine Gilcoine" }).ClickAsync();
+
+        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Unfollow Starbuck now is what we hear the worst. — 2023-08-" }).Locator("button[name=\"AuthorEmail\"]").ClickAsync();
+
+
+     }
 
     [Test]
-    public async Task LoginAndForgetMe()
-    {
+    public async Task LoginAndForgetMe() {
+       
         await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false,
@@ -93,8 +98,6 @@ public class Tests : PageTest
 
         await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
 
-        await page.GetByLabel("Username or email address").ClickAsync();
-
         await page.GetByLabel("Username or email address").FillAsync("JuliaKlompmaker");
 
         await page.GetByLabel("Username or email address").PressAsync("Tab");
@@ -102,26 +105,16 @@ public class Tests : PageTest
         await page.GetByLabel("Password").FillAsync("Gfa82mqs");
 
         await page.GetByLabel("Password").PressAsync("Enter");
-
-        await page.GotoAsync("https://bdsagroup8chirprazor.azurewebsites.net/");
-
-        await page.GetByRole(AriaRole.Link, new() { Name = "JuliaKlompmaker's profile" }).ClickAsync();
-
-        await page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
-
-        await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine Follow Starbuck now is what we hear the worst. — 2023-08-01 13:17:" }).GetByRole(AriaRole.Button).ClickAsync();
 
         await page.GetByRole(AriaRole.Link, new() { Name = "JuliaKlompmaker's profile" }).ClickAsync();
 
         await page.GetByRole(AriaRole.Button, new() { Name = "Forget Me!" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-
-        await page.GetByRole(AriaRole.Link, new() { Name = "JuliaKlompmaker's profile" }).ClickAsync();
-
     }
 
 }
+
+
 
 
 
